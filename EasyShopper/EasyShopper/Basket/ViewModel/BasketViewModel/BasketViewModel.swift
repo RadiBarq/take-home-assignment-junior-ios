177b1478d79deebd247b2cd90b025cd500d9490e
radibarq <radi.barq@harri.com>
2020-06-26 19:40:32 +0300

    Basket View Model

---
+++ EasyShopper/EasyShopper/Basket/ViewModel/BasketViewModel/BasketViewModel.swift
@@ -0,0 +1,54 @@
+//
+//  BasketViewModel.swift
+//  EasyShopper
+//
+//  Created by Harri on 6/26/20.
+//  Copyright © 2020 Ka-ching. All rights reserved.
+//
+
+
+import Foundation
+import UIKit
+
+/// ProductTableViewCellRepresentable
+class ProductTableViewCellRepresentable: TableViewCellRepresentable {
+    
+    /// Cell Height
+    var cellHeight: CGFloat
+    
+    /// Cell Representable
+    var cellReuseIdentifier: String
+    
+    /// Name
+    let name: String
+    
+    /// Image URL
+    let imageURL: String
+        
+    /**
+     Init
+     */
+    init() {
+        
+        // Set default values
+        self.cellHeight = 0.0
+        self.cellReuseIdentifier = ""
+        self.name = ""
+        self.imageURL = ""
+    }
+    
+    init(name: String, imageURL: String) {
+        
+        // Setup cell height
+        self.cellHeight = ProductTableViewCell.getCellHeight()
+        
+        // Setup cell reuse identifier
+        self.cellReuseIdentifier = ProductTableViewCell.getReuseIdentifier()
+        
+        // Set name
+        self.name = name
+        
+        // Set image url
+        self.imageURL = imageURL
+    }
+}
